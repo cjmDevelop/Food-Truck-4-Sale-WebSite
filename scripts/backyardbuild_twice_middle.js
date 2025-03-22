9055bfa08de39d23e5881c2f522311f343a39bd6
cjmDevelop <cjm.github@pm.me>
2025-03-22 08:13:58 -0400

    starting over, trying a banner-like approach to show picture and selling caption for 2-d content.

--- scripts/backyardbuild_twice_middle.js
+++ scripts/backyardbuild_twice_middle.js
@@ -3,9 +3,6 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xe3e3e3 );
-scene.fog = new THREE.Fog( 0xa0a0a0, 10, 500 );
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.2, 10);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -15,15 +12,19 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
 // renderer.setClearColor(0x000000, 0);
-renderer.setSize( window.innerWidth / 6, window.innerHeight / 6 );
-document.getElementById('middle_model').appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth / 2, window.innerHeight / 2 );
+document.getElementById('twice-2024-3dmodel').appendChild(renderer.domElement);
 
 loader.load( '/foodtruck2024.glb', function ( gltf ) {
     const model = gltf.scene;
     scene.add(model);
-    camera.position.z = 1.3;
+
+    // model.rotation.y = Math.PI;
+
+    camera.position.z = 1.7;
     camera.position.y = 0.8;
     camera.position.x = -1;
+    
 }, undefined, function ( error ) {
     console.error( error );
 });
