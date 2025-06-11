7be71aef678a89300cdda7611cd5bcb081810431
uncle doey <cjmdevelop@uncles-MacBook-Pro.local>
2025-06-11 17:17:31 -0400

    All sizing for the signs on the fence theme seems sufficient.

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.2, 10);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.2, 10);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 // renderer.setClearColor(0x000000, 0);
